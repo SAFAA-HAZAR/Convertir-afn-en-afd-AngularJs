1b0127bda278eff6f7c5ea7e9f72a00be830fa88
SAFAA-HAZAR <safaahazar700@gmail.com>
2017-01-29 20:25:32 +0100

    Add files via upload

---
+++ analyseur.h
@@ -0,0 +1,25 @@
+typedef enum {false = 0 , true =1} bool;
+
+typedef enum{
+ESPACE=1009,
+DEL=1006,
+INT=1001,
+BOOL=1002,
+TRUE=1003,
+FALSE =1004,
+IDF=1005,
+NOMBRE=1007,
+EGALE=44,
+PLUS=23,
+OUV=45,
+FER=77,
+rt_ligne=876,
+begin=55,
+END=0,
+IF=11,
+THEN=64,
+ENDIF=9,
+ELSE=88,
+Text=1008
+}tokensType;
+tokensType token;
